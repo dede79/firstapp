d3f03296b79e213020915fd1c6544ab602a4c232
andreahurren <limahawkins@gmail.com>
2015-08-22 20:21:34 +0100

    added sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,13 @@ Nameofapp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
