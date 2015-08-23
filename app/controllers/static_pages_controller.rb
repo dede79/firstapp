cdb5cf2c89c71f0bdc21fd4c7a14a59d4dd902ca
andreahurren <limahawkins@gmail.com>
2015-08-23 11:00:08 +0100

    added mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,19 @@
 class StaticPagesController < ApplicationController
 
   def landing_page
+    @featured_product = Product.first
     @products = Product.limit(3)
     @product = Product.new
   end
   
   def contact
-  	
   end
-  
-    def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver
-    end
+
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver
+  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-	  def contact_form
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver
-	end
+    def contact_form(email, name, message)
+	  @message = message
+	  mail(:from => email,
+		:to => 'limahawkins@gmail.com',
+		:subject => "A new message from #{name}")
+    end
 end
