81ae859a5ea9fbd4c7fc047f16fb76e170c4b7c0
andreahurren <limahawkins@gmail.com>
2015-08-29 09:46:39 +0100

    added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Nameofapp::Application.routes.draw do
   get "static_pages/about"
   post "static_pages/thank_you"
 
-  resources :products
+  resources :products do
+    resources :comments
+  end
+  
   resources :orders, only: [:index, :show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
