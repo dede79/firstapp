d4df692b7e2880260b6b0f5e1d395deac5694ad5
andreahurren <limahawkins@gmail.com>
2015-10-08 17:02:57 +0100

    fixed comment and rating on lesson 57

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,15 @@
 //raty rating function initializer:
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets', 
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-    
+
+
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets', 
+	readOnly: true,
+	score: function() {
+	return $(this).attr('data-score');
+	}
+	})
+  
     // zoom in on the product show images
     $(".img-zoom").elevateZoom();
 });
