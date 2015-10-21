2e647d10f2c94bfb1d3e3e9ad5934afd9ca157fd
andreahurren <limahawkins@gmail.com>
2015-10-21 20:22:56 +0100

    added heroku config for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(:host => 'localhost', :port => 6379
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
