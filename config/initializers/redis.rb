05ebcdab83840e3e769fdc207cc24eb61f74b4c9
andreahurren <limahawkins@gmail.com>
2015-10-22 22:01:15 +0100

    added redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
  uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
-    REDIS = Redis.new(:url => uri)s = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+    REDIS = Redis.new(:url => uri)
